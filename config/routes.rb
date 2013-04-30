24c85899d309ea5efb8a777ee8f7fe84322629dd
Marvin <kokororok_15@yahoo.com>
2013-04-30 15:53:18 +0800

    created index page for users

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 TimeCard::Application.routes.draw do
+
+get "/users" => "users#index"
+get "/users/new" => "users#new"
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
@@ -55,4 +58,5 @@ TimeCard::Application.routes.draw do
   # This is a legacy wild controller route that's not recommended for RESTful applications.
   # Note: This route will make all actions in every controller accessible via GET requests.
   # match ':controller(/:action(/:id))(.:format)'
+
 end
