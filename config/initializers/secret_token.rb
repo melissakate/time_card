13ec4a886750272a75f94a07a45c14584bfc3031
melissa <kateramos0293@yahoo.com>
2013-04-30 15:01:12 +0800

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TimeCard::Application.config.secret_token = 'b4a7f04d42da71f25831fece94baad5217fbf74ac83bbfa4ddd3b129fd12b4e5cd990888a0a2c9eac65483c9eaacd01ede19fe9aefb8d2557a49a1743d6d63ee'
