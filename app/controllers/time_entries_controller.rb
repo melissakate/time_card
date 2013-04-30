2e4ffb8c397c57f62570c43a7f6a6cae45131b71
melissa <kateramos0293@yahoo.com>
2013-04-30 18:26:17 +0800

    timecard2

--- app/controllers/time_entries_controller.rb
+++ app/controllers/time_entries_controller.rb
@@ -2,4 +2,16 @@ class TimeEntriesController < ApplicationController
   def index
     @time_entries = TimeEntry.all
   end
+
+  def create
+    t= TimeEntry.new
+    t.date = params[:date]
+    t.time_in = params[:time_in]
+    t.time_out = params[:time_out]
+    t.accomplishments = params[:accomplishments]
+    t.remarks = params[:remarks]
+    t.save
+    redirect_to action: "index"
+  end
 end
+
--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,8 @@ TimeCard::Application.routes.draw do
   get"/time_entries/new" => "time_entries#new"
   get "/users" => "users#index"
   get "/users/new" => "users#new"
+
+
+  post"/time_entries" => "time_entries#create"
+
 end
