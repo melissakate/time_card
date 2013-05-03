ed94c07c7a3b703a6049c1837da95323ee386aa7
melissa <kateramos0293@yahoo.com>
2013-05-03 15:53:46 +0800

    resources

--- config/routes.rb
+++ config/routes.rb
@@ -1,28 +1,29 @@
 TimeCard::Application.routes.draw do
+  resources :time_entries
 
 
-  post "/users" => "users#create" 
+ # post "/users" => "users#create" 
   # sa view: create display submitted using form 
-  get"/time_entries" => "time_entries#index"
-  get"/time_entries/new" => "time_entries#new"
-  get "/users" => "users#index"
-  get "/users/new" => "users#new"
+  #get"/time_entries" => "time_entries#index"
+  #get"/time_entries/new" => "time_entries#new"
+  #get "/users" => "users#index"
+  #get "/users/new" => "users#new"
   
-  get "/users/:id//edit" => "users#edit"
-  put "/users/:id" => "users#update"
-  get "/users/:id" => "users#show"
+  #get "/users/:id//edit" => "users#edit"
+  #put "/users/:id" => "users#update"
+  #get "/users/:id" => "users#show"
 
-  post"/time_entries" => "time_entries#create"
-  delete "users/:id" => "users#delete"
-  post "/a"  => "users#a"
-
-
-get "/time_entries/:id/edit" => "time_entries#edit"
-put "/time_entries/:id" => "time_entries#update"
-get "/time_entries/:id" => "time_entries#show"
+  #post"/time_entries" => "time_entries#create"
+  #delete "users/:id" => "users#delete"
+  #post "/a"  => "users#a"
 
 
-  delete "/time_entries/:id" => "time_entries#destroy"
+#get "/time_entries/:id/edit" => "time_entries#edit"
+#put "/time_entries/:id" => "time_entries#update"
+#get "/time_entries/:id" => "time_entries#show"
 
-  post "/c1" => "time_entries#c1"
+
+ # delete "/time_entries/:id" => "time_entries#destroy"
+
+  #post "/c1" => "time_entries#c1"
 end
