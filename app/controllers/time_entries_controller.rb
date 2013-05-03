2ad8872d90f8b5b8d96ac87dff1870a61550089e
melissa <kateramos0293@yahoo.com>
2013-05-03 14:42:44 +0800

    time card update

--- app/controllers/time_entries_controller.rb
+++ app/controllers/time_entries_controller.rb
@@ -13,5 +13,48 @@ class TimeEntriesController < ApplicationController
     t.save
     redirect_to action: "index"
   end
+
+
+def show
+  @time_entry = TimeEntry.find(params[:id])
+end
+  def edit
+    id = params[:id]
+    @time_entry = TimeEntry.find(id)
+  end
+def c1 
+
+  t= TimeEntry.new
+    t.date = params[:date]
+    t.time_in = params[:time_in]
+    t.time_out = params[:time_out]
+    t.accomplishments = params[:accomplishments]
+    t.remarks = params[:remarks]
+    t.save
+    redirect_to action: "index"
+end
+def new
+  @time_entry = TimeEntry.new
+end
+
+
+def destroy
+  time_entry = TimeEntry.find(params[:id])
+  time_entry.destroy
+    redirect_to action: "index"
+end
+  def update
+    id = params[:id]
+    @time_entry = TimeEntry.find(id)
+
+    t= TimeEntry.find(id)
+    t.date = params[:date]
+    t.time_in = params[:time_in]
+    t.time_out = params[:time_out]
+    t.accomplishments = params[:accomplishments]
+    t.remarks = params[:remarks]
+    #t.save
+  end
+
 end
 
--- config/routes.rb
+++ config/routes.rb
@@ -7,4 +7,13 @@ TimeCard::Application.routes.draw do
 
   post"/time_entries" => "time_entries#create"
 
+
+get "/time_entries/:id/edit" => "time_entries#edit"
+put "/time_entries/:id" => "time_entries#update"
+get "/time_entries/:id" => "time_entries#show"
+
+
+  delete "/time_entries/:id" => "time_entries#destroy"
+
+  post "/c1" => "time_entries#c1"
 end
