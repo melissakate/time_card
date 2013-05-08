d5df675e6d84f545107ea94874fcf333af0dcc77
Kenneth <github@kennethcalamay.com>
2013-05-08 11:27:19 +0800

    Cleaned time entries controller

--- app/controllers/time_entries_controller.rb
+++ app/controllers/time_entries_controller.rb
@@ -10,20 +10,19 @@ class TimeEntriesController < ApplicationController
 
   def new
     @time_entry = TimeEntry.new
-    @time_entry.time_in = Time.now.strftime("%I:%H %p")
-    @time_entry.time_out = Time.now.strftime("%I:%H %p")
+    @time_entry.time_in = Time.now
   end
 
   def edit
-    id = params[:id]
-    @time_entry = TimeEntry.find(id)
+    @time_entry = TimeEntry.find params[:id]
   end
 
   def create
     t = TimeEntry.new(params[:time_entry])
     t.save
+
     flash[:notice] = "create"
-    redirect_to action: "index"
+    redirect_to time_entry_path(t)
   end
 
   def update
@@ -31,13 +30,14 @@ class TimeEntriesController < ApplicationController
     @time_entry.update_attributes(params[:time_entry])
 
     flash[:notice]="records updated"
-    redirect_to action: "show", id: @time_entry.id
+    redirect_to time_entry_path(@time_entry)
   end
 
   def destroy
     time_entry = TimeEntry.find(params[:id])
     time_entry.destroy
-    redirect_to action: "index"
+
+    redirect_to time_entries_path
   end
 
 end
