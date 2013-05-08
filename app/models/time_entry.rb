514e3cea090defe5e2ef4de2f63c1f9c0cf8573b
melissa <kateramos0293@yahoo.com>
2013-05-08 11:37:50 +0800

    Fixed bugs in user and time entry model

--- app/models/time_entry.rb
+++ app/models/time_entry.rb
@@ -1,6 +1,7 @@
 class TimeEntry < ActiveRecord::Base
-  attr_accessible :user_id :accomplishments, :date, :remarks, :time_in, :time_out
-  belongs_to:User :user_id
+  attr_accessible :user_id, :accomplishments, :date, :remarks, :time_in, :time_out
+
+  belongs_to :user
 
   def time_in
     value = read_attribute(:time_in)
